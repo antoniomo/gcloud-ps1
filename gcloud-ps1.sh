8ae47a24080d23bad746fa1da67f23e81bd9ec8f
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2019-12-10 02:30:49 +0200

    Zsh compatible colors

--- gcloud-ps1.sh
+++ gcloud-ps1.sh
@@ -7,10 +7,10 @@ GCLOUD_PS1_ENABLE="${GCLOUD_PS1_ENABLE:-true}"
 GCLOUD_PS1_SYMBOL_ENABLE="${GCLOUD_PS1_SYMBOL_ENABLE:-true}"
 GCLOUD_PS1_SYMBOL="${GCLOUD_PS1_SYMBOL:-$'\u2601'}"
 
-# http://mywiki.wooledge.org/BashFAQ/037
-reset="\\[$(tput sgr0)\\]"    # Text Reset
-green="\\[$(tput setaf 2)\\]" # Green
-blue="\\[$(tput setaf 4)\\]"  # Blue
+# Terminal colors
+reset='\033[0m'
+blue='\033[1m\033[34m'
+green='\033[1m\033[32m'
 
 gcloudon() {
 	GCLOUD_PS1_ENABLE=true
@@ -27,7 +27,7 @@ gcloud_ps1() {
 		if [[ ${GCLOUD_PS1_SYMBOL_ENABLE} == true ]]; then
 			GCLOUD_PS1+="${blue}${GCLOUD_PS1_SYMBOL} "
 		fi
-		ACTIVE_CONF=$(${GCLOUD_BIN} config configurations list --format='value(name)' --filter='IS_ACTIVE=true' 2>/dev/null)
+		ACTIVE_CONF=$(${GCLOUD_BIN} config configurations list --format='value(name)' --filter='IS_ACTIVE=true' 2> /dev/null)
 		GCLOUD_PS1+="${green}$ACTIVE_CONF${reset})"
 	fi
 
